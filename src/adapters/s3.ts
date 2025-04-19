936deacae5bee517c42a2c509e7ee07fd146f4eb
GwerhDev <gwerh.dev@gmail.com>
2025-04-19 15:08:47 -0400

    cleaning methods

--- src/adapters/s3.ts
+++ src/adapters/s3.ts
@@ -1,6 +1,7 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3Client, PutObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3';
 import { StorageAdapter, S3Config } from '../types';
+import bcrypt from 'bcrypt';
 
 export function createS3Adapter(config: S3Config): StorageAdapter {
   const s3 = new S3Client({
@@ -12,8 +13,10 @@ export function createS3Adapter(config: S3Config): StorageAdapter {
   });
 
   return {
-    async getPresignedUrl(filename, contentType, projectId) {
-      const key = `${projectId}/file-${Date.now()}-${filename}`;
+    async getPresignedUrl(filename: string, contentType: string, projectId: string) {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(filename, salt);
+      const key = `${projectId}/file-${Date.now()}-${hashedPassword}`;
       const command = new PutObjectCommand({
         Bucket: config.bucket,
         Key: key,
@@ -21,7 +24,10 @@ export function createS3Adapter(config: S3Config): StorageAdapter {
       });
 
       const url = await getSignedUrl(s3, command, { expiresIn: 60 });
-      return { url, key };
+
+      const publicUrl = `https://${config.bucket}.s3.${config.region}.amazonaws.com/${key}`;
+
+      return { url, publicUrl };
     },
 
     async listFiles(projectId: string) {
