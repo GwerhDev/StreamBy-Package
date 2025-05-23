c0f4bac34f52c4cb27a7a3fe9cc4da29aee4896b
GwerhDev <gwerh.dev@gmail.com>
2025-05-23 12:01:24 -0400

    updating version

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createStreamByRouter } from './middleware/createRouter';
 import { StreamByConfig, StorageAdapter } from './types';
 
+export { createS3Adapter } from './adapters/s3';
 export { initProjectModel } from './db/initProjectModel';
 export { createStreamByRouter } from './middleware/createRouter';
 export { createMongoProjectProvider } from './providers/mongoProjectProvider';
